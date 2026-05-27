4174d864fbd363495047dda9babef76bdb975106
ColosYoung <chanchaichakam1997@gmail.com>
2026-05-27 16:12:42 +0700

    feat: configure next-intl plugin and remote image patterns in next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,21 @@
+import type { NextConfig } from "next";
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntlPlugin();
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  experimental: {
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.public.blob.vercel-storage.com',
+      },
+    ],
+  },
+  allowedDevOrigins: ['192.168.1.142', '192.168.1.130'],
+};
+
+export default withNextIntl(nextConfig);
